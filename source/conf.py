c1866abeecc1f82c3d5d60d038fe7ba29f8b752b
core-man <coreman.seism@gmail.com>
2020-09-06 22:45:38 +0800

    update conf.py

--- source/conf.py
+++ source/conf.py
@@ -31,6 +31,7 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -101,9 +102,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
-#import sphinx_rtd_theme
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'default'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
