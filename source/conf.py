d1bd16f189a15b38493c9eab8f675ca802408443
Yao Jiayuan <coreman.seism@gmail.com>
2021-05-13 21:00:49 +0800

    Tiny fix (#122)

--- source/conf.py
+++ source/conf.py
@@ -36,7 +36,7 @@ rst_prolog = """
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.githubpages'
+    "sphinx.ext.githubpages"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
